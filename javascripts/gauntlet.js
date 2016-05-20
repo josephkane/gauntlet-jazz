eae68da90c167517b4605a0277c36c8ba474049e
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-20 14:42:15 -0500

    Basic character creation and reorganization

--- javascripts/gauntlet.js
+++ javascripts/gauntlet.js
@@ -1,16 +1,12 @@
 "use strict";
 
-var Gauntlet = (function(gauntlet) {
-  //NOTE(adam): this would attach to a character to give it a weapon
-  gauntlet.constructWeapon = function(weaponId) {
-    //NOTE(adam): create generic weapon
-    var weapon = new gauntlet.Weapon();
-    //NOTE(adam): attach specific properties to weapon
-    Object.assign(weapon, gauntlet.getWeaponData(weaponId));
-    return weapon;
+(function() {
+  let details = {
+    name: "Steve",
+    species: "orc",
+    class: "warrior",
+    weapon: "warAxe"
   };
 
-  return gauntlet;
-}(Gauntlet || {}));
-
-console.log("Gauntlet.constructWeapon(warAxe)", Gauntlet.constructWeapon("warAxe"));
+  console.log("character", Gauntlet.Character.constructCharacter(details));
+}());
