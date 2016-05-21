8366ae6d1e83f4b76a2dd52417441ce93c325222
Sulaiman.Allen <xalerons@gmail.com>
2016-05-21 12:45:34 -0500

    Implemented random spell generation

--- javascripts/spells.js
+++ javascripts/spells.js
@@ -9,12 +9,27 @@ var Gauntlet = (function(gauntlet) {
     let spell = new gauntlet.Spell();
     Object.assign(spell, spells[spellId]);
     return spell;
+  }; 
+
+  gauntlet.Spell.random = function() {
+    // chose spelled based on randomly indexing into spells object.
+    var spellsArray = Object.keys(spells);
+    var selectedSpell = spellsArray[Math.floor(Math.random()*spellsArray.length)];
+    let spell = new gauntlet.Spell();
+    Object.assign(spell, spells[selectedSpell]);
+    return spell;
   };
+
 
   let spells = {
     sphere: {
       name: "Sphere",
       damageCalc: Math.floor(Math.random() * 10 + 10),  //TODO(adam): convert to function
+      type: "random"
+    },
+    firewall: {
+      name: "Fire Wall",
+      damageCalc: Math.floor(Math.random() * 12 + 14),  //TODO(adam): convert to function
       type: "random"
     }
   };
@@ -22,4 +37,6 @@ var Gauntlet = (function(gauntlet) {
   return gauntlet;
 }(Gauntlet || {}));
 
-console.log("Gauntlet.Spell.constructSpell(sphere)", Gauntlet.Spell.constructSpell("sphere"));
+
+console.log("Gauntlet.Spell.random() =  ", Gauntlet.Spell.random());
+// console.log("Gauntlet.Spell.constructSpell(sphere)", Gauntlet.Spell.constructSpell("sphere"));
