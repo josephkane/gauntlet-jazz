061e41441b143b12f73903c5b6fd3ad164c7304a
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-21 13:03:56 -0500

    Working basic combat

--- javascripts/characters.js
+++ javascripts/characters.js
@@ -3,6 +3,12 @@
 var Gauntlet = (function(gauntlet) {
   gauntlet.Character = function(name) {
     this.name = name;
+    this.health = 100;
+  };
+
+  gauntlet.Character.prototype.attack = function(enemy) {
+    enemy.health -= this.weapon.damage;
+    console.log("Attack: ", `${this.name} attacks ${enemy.name} with ${this.weapon.name} for ${this.weapon.damage} damage.`);
   };
 
   gauntlet.Character.constructCharacter = function(details) {
@@ -11,6 +17,8 @@ var Gauntlet = (function(gauntlet) {
     character.species = gauntlet.Species.constructSpecies(details.speciesId);
     character.class = gauntlet.PlayerClass.constructClass(details.classId);
     character.weapon = gauntlet.Weapon.constructWeapon(details.weaponId);
+
+    character.health += character.class.healthBonus;
 
     return character;
   };
--- javascripts/gauntlet.js
+++ javascripts/gauntlet.js
@@ -1,12 +1,44 @@
 "use strict";
 
-(function() {
-  let details = {
+var Gauntlet = (function(gauntlet) {
+  let player = null;
+  let enemy = null;
+
+  gauntlet.getPlayer = () => player;
+  gauntlet.setPlayer = (playerData) =>
+    player = Gauntlet.Character.constructCharacter(playerData);
+  gauntlet.getEnemy = () => enemy;
+  gauntlet.setEnemy = (enemyData) =>
+    enemy = Gauntlet.Character.constructCharacter(enemyData);
+
+  gauntlet.fight = function() {
+    player.attack(enemy);
+    enemy.attack(player);
+
+    //TODO(adam): win/lose checking
+  };
+
+  return gauntlet;
+}(Gauntlet || {}));
+
+Gauntlet.setPlayer({
+    name: "Jim",
+    speciesId: "human",
+    classId: "berserker",
+    weaponId: "broadsword"
+  });
+
+Gauntlet.setEnemy({
     name: "Steve",
     speciesId: "orc",
     classId: "warrior",
     weaponId: "warAxe"
-  };
+  });
 
-  console.log("character", Gauntlet.Character.constructCharacter(details));
-}());
+Gauntlet.fight();
+Gauntlet.fight();
+Gauntlet.fight();
+Gauntlet.fight();
+
+console.log("Gauntlet.getPlayer()", Gauntlet.getPlayer());
+console.log("Gauntlet.getEnemy()", Gauntlet.getEnemy());
