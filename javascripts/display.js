4410befb922e0b24357e5b9e4972e4214c8ae1ca
Sulaiman.Allen <xalerons@gmail.com>
2016-05-23 09:34:32 -0500

    Disregard this

--- javascripts/display.js
+++ javascripts/display.js
@@ -5,15 +5,16 @@
   var jim = Gauntlet.getPlayer();
   var steve = Gauntlet.getEnemy();
   console.log("jim = ", jim);
-  $("#player-1-name").html(`Name : ${jim.name}`);
-  $("#player-1-species").html(`Species : ${jim.species.name}`);
-  $("#player-1-hp").html(`HP : ${jim.health}`);
-  $("#player-1-weapon").html(`Weapon : ${jim.weapon.name}`);
+  
+  $(".player-name").html(`Name : ${jim.name}`);
+  $(".player-species").html(`Species : ${jim.species.name}`);
+  $(".player-hp").html(`HP : ${jim.health}`);
+  $(".player-weapon").html(`Weapon : ${jim.weapon.name}`);
 
-  $("#player-2-name").html(`Name : ${steve.name}`);
-  $("#player-2-species").html(`Species : ${steve.species.name}`);
-  $("#player-2-hp").html(`HP : ${steve.health}`);
-  $("#player-2-weapon").html(`Weapon : ${steve.weapon.name}`);
+  $(".enemy-name").html(`Name : ${steve.name}`);
+  $(".enemy-species").html(`Species : ${steve.species.name}`);
+  $(".enemy-hp").html(`HP : ${steve.health}`);
+  $(".enemy-weapon").html(`Weapon : ${steve.weapon.name}`);
 }());
 
 // $("player-1-name").html(details.name);
