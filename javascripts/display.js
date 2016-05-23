9006df542bd247e85a8f2834c8c3e41e0ec618fe
Sulaiman.Allen <xalerons@gmail.com>
2016-05-23 09:44:16 -0500

    Removed Ids from html

--- javascripts/display.js
+++ javascripts/display.js
@@ -6,15 +6,15 @@
   var steve = Gauntlet.getEnemy();
   console.log("jim = ", jim);
   
-  $(".player-name").html(`Name : ${jim.name}`);
-  $(".player-species").html(`Species : ${jim.species.name}`);
-  $(".player-hp").html(`HP : ${jim.health}`);
-  $(".player-weapon").html(`Weapon : ${jim.weapon.name}`);
+  $(".player-name").html(` ${jim.name}`);
+  $(".player-species").html(` ${jim.species.name}`);
+  $(".player-hp").html(` ${jim.health}`);
+  $(".player-weapon").html(` ${jim.weapon.name}`);
 
-  $(".enemy-name").html(`Name : ${steve.name}`);
-  $(".enemy-species").html(`Species : ${steve.species.name}`);
-  $(".enemy-hp").html(`HP : ${steve.health}`);
-  $(".enemy-weapon").html(`Weapon : ${steve.weapon.name}`);
+  $(".enemy-name").html(` ${steve.name}`);
+  $(".enemy-species").html(` ${steve.species.name}`);
+  $(".enemy-hp").html(` ${steve.health}`);
+  $(".enemy-weapon").html(` ${steve.weapon.name}`);
 }());
 
 // $("player-1-name").html(details.name);
