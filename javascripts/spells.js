fff196b2fc8db54a1f66df1e022bdd5c5f54671c
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-23 09:33:22 -0500

    Create function to get random id from array

--- javascripts/spells.js
+++ javascripts/spells.js
@@ -3,13 +3,14 @@
 var Gauntlet = (function(gauntlet) {
   let spellTypes = ["lightning", "fire", "water", "earth", "mysticism"];
 
+  let getRandId = (idArray) => idArray[Math.floor(Math.random()*idArray.length)];
+
   gauntlet.Spell = function() {};
 
   gauntlet.Spell.constructSpell = function(spellId) {
     let spell = new gauntlet.Spell();
     if(spellId === "random") {
-      let spellsArray = Object.keys(spells);
-      spellId = spellsArray[Math.floor(Math.random()*spellsArray.length)];
+      spellId = getRandId(Object.keys(spells));
     }
     Object.assign(spell, spells[spellId]);
     return spell;
