68e740dccd90404c4c88b6877ae44a06f0be0d29
Joseph Kane <josephckane@gmail.com>
2016-05-23 14:37:54 -0500

    combat field turned white, background image added to combat screen

--- javascripts/display.js
+++ javascripts/display.js
@@ -2,38 +2,19 @@
 
 (function() {
 
-  $(document).ready(function () {
-    $("#combat-screen").hide()
-  });
+  var jim = Gauntlet.getPlayer();
+  var steve = Gauntlet.getEnemy();
+  console.log("jim = ", jim);
 
-  $("#fight-button").click(function () {
-    $("#select-screen").hide();
-    $("#combat-screen").show();
-  });
+  $(".player-name").html(` ${jim.name}`);
+  $(".player-species").html(` ${jim.species.name}`);
+  $(".player-hp").html(` ${jim.health}`);
+  $(".player-weapon").html(` ${jim.weapon.name}`);
 
-  $("#attack-button").click(function () {
-    Gauntlet.fight();
-    updateStats();
-  });
-
-  var updateStats = function () {
-
-    var jim = Gauntlet.getPlayer();
-    var steve = Gauntlet.getEnemy();
-    console.log("jim = ", jim);
-
-    $(".player-name").html(` ${jim.name}`);
-    $(".player-species").html(` ${jim.species.name}`);
-    $(".player-hp").html(` ${jim.health}`);
-    $(".player-weapon").html(` ${jim.weapon.name}`);
-
-    $(".enemy-name").html(` ${steve.name}`);
-    $(".enemy-species").html(` ${steve.species.name}`);
-    $(".enemy-hp").html(` ${steve.health}`);
-    $(".enemy-weapon").html(` ${steve.weapon.name}`);
-
-  }
-
+  $(".enemy-name").html(` ${steve.name}`);
+  $(".enemy-species").html(` ${steve.species.name}`);
+  $(".enemy-hp").html(` ${steve.health}`);
+  $(".enemy-weapon").html(` ${steve.weapon.name}`);
 }());
 
 // $("player-1-name").html(details.name);
