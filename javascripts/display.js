81afbe6763576b5dcf8bb7c9d5d4ecf5eceddfbe
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-23 20:31:10 -0500

    Coloring of disabled inputs. Fix not disabling fight button on species or class change. Autofocus name input.

--- javascripts/display.js
+++ javascripts/display.js
@@ -65,12 +65,14 @@
     $classSelect.selectedIndex = -1;
     $weaponSelect[0].disabled = true;
     $weaponSelect.selectedIndex = -1;
+    $fightButton[0].disabled = true;
   });
 
   $classSelect.change(function(e) {
     populateWeapons(e.target.value);
     $weaponSelect[0].disabled = false;
     $weaponSelect.selectedIndex = -1;
+    $fightButton[0].disabled = true;
   });
 
   $weaponSelect.change(function() {
@@ -98,5 +100,7 @@
     $("#combat-screen").show();
     updateStats();
   });
+
+  $nameInput.focus();
 
 }());
